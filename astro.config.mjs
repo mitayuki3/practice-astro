2f7945f2e39827640fe4dee5c6083ff2a359ad49
Yuki MITA <mitayuki3@yahoo.co.jp>
2024-10-10 23:31:43 +0900

    デプロイ設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://mitayuki3.github.io",
+  base: "/practice-astro",
+});
